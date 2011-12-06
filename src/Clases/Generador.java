c93e5a0fe680bde131560cff2e5d11c8a304e103
leogtzr <leogutierrezramirez@gmail.com>
2011-12-05 22:21:55 -0700

    Comentarios innecesarios en Generador.java

--- src/Clases/Generador.java
+++ src/Clases/Generador.java
@@ -74,8 +74,6 @@ public class Generador {
             e.printStackTrace();
         } finally {
             try {
-           // Nuevamente aprovechamos el finally para 
-           // asegurarnos que se cierra el fichero.
            if (null != fichero)
               fichero.close();
            } catch (Exception e2) {
