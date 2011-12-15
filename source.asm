9ee42a4858b0d7827e1e53d7368f7cf81b8addad
Alex Kuster <vertexSymphony@zoho.com>
2011-12-15 01:53:26 -0300

    Limpieza general del tree
    Signed-off-by: Alex Kuster <vertexSymphony@zoho.com>

--- source.asm
+++
@@ -1,9 +0,0 @@
-; Assembly code para el programa "simple.glhbr"
-MOV R0,#18 ; Cargamos 18
-MOV R1,#3 ; Cargamos 3
-DIV R0,R1     ; Dividimos
-MOV R1,#1 ; Cargamos 1
-ADD R0,R1 ; Sumamos
-MOV x,R0    ; fin de la sentencia
-; Rutina para mostrar el resultado de x
-;        R = 7.0
